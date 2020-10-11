785bae7100acbb41af1457a13f6c4db2cf98567d
riefer02 <andrew.riefenstahl@gmail.com>
2020-10-11 18:44:44 -0500

    vue-meta tags and SEO Optimization

---
+++ pages/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create a Booking</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create a Booking',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new booking at Casa Patron',
+        },
+      ],
+    }
+  },
+}
+</script>
